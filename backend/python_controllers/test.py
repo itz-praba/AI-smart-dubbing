b599163e8e7c3add2cd22890e17d2b3760523cc3
Dinesh prabakaran <dineshprabakaran2509@gmail.com>
2026-02-19 00:41:06 +0530

    delete

--- backend/python_controllers/test.py
+++
@@ -1,8 +0,0 @@
-from pyannote.audio import Pipeline
-
-pipeline = Pipeline.from_pretrained(
-    "pyannote/speaker-diarization-3.1",
-    token="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"
-)
-
-pipeline.save_pretrained("D:/AI_Models/pyannote/speaker-diarization-3.1")
